8adedc31af58e8a5ea8b68c43dd508d74dbd3744
Álvaro_Silva <alvaro18555silva@gmail.com>
2024-08-12 12:02:27 +0100

    primeiro comit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greenting', function (){
+
+
+return 'Hello World'; 
+
+}); 
